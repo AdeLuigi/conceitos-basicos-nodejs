8cd2508d8e8d2354e8ee0c628065149f8a6a501f
Ademario Vitor <ademario63@gmail.com>
2020-04-07 16:37:13 -0300

    finalizacao dos conceitos

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,96 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  if(!isUuid(id)){
+    return response.status(400).json({message: "Mano, deu ruim nesse id"});
+  }
+  
+  repositoryIndex = repositories.findIndex((repository) =>  repository.id === id )
+
+  if(repositoryIndex < 0){
+    return response.status(404).json({message:"Esse id é valido mas não tá no array"})
+  }
+
+  repositoryUpdate = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories.push(repositoryUpdate);
+
+  return response.status(200).json(repositoryUpdate);
+
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({message: "Mano, deu ruim nesse id"});
+  }
+  
+  repositoryIndex = repositories.findIndex((repository) =>  repository.id === id )
+
+  if(repositoryIndex < 0){
+    return response.status(404).json({message:"Esse id é valido mas não tá no array"});
+  }
+  console.log(repositories.length)
+  repositories.splice(repositoryIndex, 1);
+  console.log(repositories.length)
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({message: "Mano, deu ruim nesse id"});
+  }
+  
+  repositoryIndex = repositories.findIndex((repository) =>  repository.id === id )
+
+  if(repositoryIndex < 0){
+    return response.status(404).json({message:"Esse id é valido mas não tá no array"})
+  }
+
+  repositories[repositoryIndex].likes += 1;
+
+  return response.status(200).json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
